ea2a8a61eba393f9ba820a5e935c2aa46517c1d0
tomu <kusano.wireless@gmail.com>
2019-08-11 12:13:14 +0000

    Modify variables.tf

--- variables.tf
+++ variables.tf
@@ -3,12 +3,6 @@ variable "location" {
   default     = "East US"
 }
 
-variable "system_name" {
-  description = "System name for specifying the name of resource group"
-  default     = "scraper"
-}
-
 variable "resource_group_name" {
   description = "The resource group name"
-  default     = "scraper-rg"
 }
